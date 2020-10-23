2430752018bd5d0227812a09daf09c5c93a30760
Christian <christian.dehek@gmail.com>
2020-10-23 12:52:27 -0600

    added comment routes/comment associations

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -21,10 +21,18 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        include: [
+        include: [  
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
