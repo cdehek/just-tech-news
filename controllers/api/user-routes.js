173fd1ff99c3941f22ded0fdb3f651aede08da18
Christian <christian.dehek@gmail.com>
2020-10-27 16:29:09 -0600

    fixed typerror and finished login/logout features

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -104,7 +104,7 @@ router.post('/login', (req, res) => {
 
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
-        res.session.destroy(() => {
+        req.session.destroy(() => {
             res.status(204).end();
         });
     }
